818ad5d7db0b106aa92620448b05458dbf1c8d6e
Tishalovich Maxim <tishalovichm@gmail.com>
2024-03-26 21:07:00 +0300

    Feat: struct for storing lighting coeffs

---
+++ Cga/Graphics/LightCoeffs.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cga.Graphics;
+
+internal struct LightCoeffs
+{
+    float ka;
+    float kd;
+    float ks;
+    float shiny;
+
+    public LightCoeffs(float ka, float kd, float ks, float shiny)
+    {
+        this.ka = ka;
+        this.kd = kd;
+        this.ks = ks;
+        this.shiny = shiny;
+    }
+}
