b28154593591fa832f0c84a69b61059a122d531e
Tishalovich Maxim <tishalovichm@gmail.com>
2024-03-28 11:39:06 +0300

    Feat: vertex structure

---
+++ Cga/Graphics/Vertex.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cga.Graphics;
+
+public struct Vertex
+{
+    public Vector3 ScreenPos;
+    public Vector3 WorldPos;
+    public Vector3 Normal;
+}
