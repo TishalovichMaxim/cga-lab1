5cc8fe33f54a9dcb5e787284a39767871d6c329f
Tishalovich Maxim <tishalovichm@gmail.com>
2024-05-10 14:52:38 +0300

    Add tbn matrix calculation.

--- Cga/Graphics/Face.cs
+++ Cga/Graphics/Face.cs
@@ -1,3 +1,5 @@
+using GlmNet;
+
 namespace Cga.Graphics;
 
 public struct Face
@@ -7,4 +9,37 @@ public struct Face
     public int[] NormIndices;
     
     public int[] TextIndices;
+
+    private mat3 TbnMatrix;
+
+    public void CalculateTbn(Mesh mesh)
+    {
+        vec4 v1 = mesh.Vertices[VertIndices[0]];
+        vec4 v2 = mesh.Vertices[VertIndices[1]];
+        vec4 v3 = mesh.Vertices[VertIndices[2]];
+
+        vec3 t1 = mesh.Textures[TextIndices[0]];
+        vec3 t2 = mesh.Textures[TextIndices[1]];
+        vec3 t3 = mesh.Textures[TextIndices[2]];
+
+        vec3 dt12 = t2 - t1;
+        vec3 dt13 = t3 - t1;
+
+        float u12 = dt12.x;
+        float u13 = dt13.x;
+
+        float v12 = dt12.y;
+        float v13 = dt13.y;
+            
+        vec3 e12 = new vec3(v2 - v1);
+        vec3 e13 = new vec3(v3 - v1);
+
+        vec3 norm = glm.cross(e12, e13);
+        
+        vec3 tang = (e13*v12 - e12*v13)/(u13*v12 - u12*v13);
+
+        vec3 bitang = (e12 - u12 * tang) / v12;
+
+        TbnMatrix = new mat3(tang, bitang, norm);
+    }
 }
